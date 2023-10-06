21e0c7cc39511259d29dc62fa3b8c7854062d7ff
Serpo Lama <serpolama7@gmail.com>
2023-10-06 15:32:24 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './mycomponents/pages/index/index.component';
+import { AboutComponent } from './mycomponents/pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: IndexComponent},
+  {path: "about", component: AboutComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
