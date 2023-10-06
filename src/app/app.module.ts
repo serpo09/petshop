6feeec2a6dc29d05d5713bf94a2fc5280e7d7430
Serpo Lama <serpolama7@gmail.com>
2023-10-06 15:10:56 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { IndexComponent } from './mycomponents/pages/index/index.component';
+import { NavComponent } from './mycomponents/nav/nav.component';
+import { FooterComponent } from './mycomponents/footer/footer.component';
+import { AboutComponent } from './mycomponents/pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
